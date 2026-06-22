69e0a071ea49839f528a7b06a62d740610b4cdb6
tofoss <tormod@torcor.dev>
2026-06-22 13:22:19 +0200

    chore: refactor GetServiceAccounts

--- pkg/serviceaccounts/service.go
+++ pkg/serviceaccounts/service.go
@@ -8,6 +8,7 @@ import (
 	"github.com/navikt/union-api/pkg/k8s"
 	"github.com/navikt/union-api/pkg/middleware"
 	"github.com/navikt/union-api/pkg/uctl"
+	corev1 "k8s.io/api/core/v1"
 )
 
 type Service struct {
@@ -31,38 +32,51 @@ func (s Service) GetServiceAccounts(ctx context.Context, principal *middleware.P
 
 	var serviceAccounts []ServiceAccount
 
-	for _, permission := range permissions {
-		for _, resource := range permission.Resources {
-			if resource.Kind != uctl.Project {
-				continue
-			}
-			ns, err := resource.Namespace()
-			if err != nil {
-				return nil, err
-			}
+	for _, resource := range projectResources(permissions) {
+		ns, err := resource.Namespace()
+		if err != nil {
+			return nil, err
+		}
 
-			k8sServiceAccounts, err := s.k8sClient.ServiceAccounts(ctx, ns)
-			if err != nil {
-				slog.Error("faild to fetch kubernetes service accounts", "error", err)
-				return nil, fmt.Errorf("faild to fetch kubernetes service accounts")
-			}
-			for _, sa := range k8sServiceAccounts.Items {
-				if sa.Name == "default" {
-					continue
-				}
-				gsa, ok := sa.Annotations["iam.gke.io/gcp-service-account"]
-				if !ok {
-					continue
-				}
-				serviceAccounts = append(serviceAccounts, ServiceAccount{
-					K8sServiceAccount:    sa.Name,
-					GoogleServiceAccount: gsa,
-					UnionProject:         resource.Project,
-					UnionDomain:          resource.Domain,
-				})
+		k8sServiceAccounts, err := s.k8sClient.ServiceAccounts(ctx, ns)
+		if err != nil {
+			slog.Error("failed to fetch kubernetes service accounts", "error", err)
+			return nil, fmt.Errorf("failed to fetch kubernetes service accounts")
+		}
+		for _, k8sSa := range k8sServiceAccounts.Items {
+			if sa, ok := toServiceAccount(k8sSa, resource); ok {
+				serviceAccounts = append(serviceAccounts, sa)
 			}
 		}
 	}
 
 	return serviceAccounts, nil
+}
+
+func projectResources(permissions []uctl.Permission) []uctl.Resource {
+	var resources []uctl.Resource
+	for _, permission := range permissions {
+		for _, resource := range permission.Resources {
+			if resource.Kind == uctl.Project {
+				resources = append(resources, resource)
+			}
+		}
+	}
+	return resources
+}
+
+func toServiceAccount(k8sSa corev1.ServiceAccount, resource uctl.Resource) (ServiceAccount, bool) {
+	if k8sSa.Name == "default" {
+		return ServiceAccount{}, false
+	}
+	gsa, ok := k8sSa.Annotations["iam.gke.io/gcp-service-account"]
+	if !ok {
+		return ServiceAccount{}, false
+	}
+	return ServiceAccount{
+		K8sServiceAccount:    k8sSa.Name,
+		GoogleServiceAccount: gsa,
+		UnionProject:         resource.Project,
+		UnionDomain:          resource.Domain,
+	}, true
 }
